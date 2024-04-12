3301489029e8abcc38aadbc40b9721082ff651dc
Dmytro Prokhach <dprokhach@gmail.com>
2024-04-12 19:20:30 +0300

    Done with errors

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
+from django.views import generic
 from taxi.models import Driver, Car, Manufacturer
 
 
@@ -12,4 +13,38 @@ def index(request):
         "num_manufacturers": Manufacturer.objects.count(),
     }
 
-    return render(request, "taxi/index.html", context=context)
+    return render(request,
+                  "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.all().order_by("name")
+    paginate_by = 5
+    template_name = "taxi/manufacturer_list.html"
+
+
+class CarListView(generic.ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
+    template_name = "taxi/car_list.html"
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+    template_name = "taxi/car_detail.html"
+    queryset = (Car.objects.
+                prefetch_related("manufacturer", "drivers"))
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+    template_name = "taxi/driver_list.html"
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+    queryset = Driver.objects.prefetch_related("cars")
